09e07291b26dc7c73168fe3a4876ad74f871fbe7
Justin Wong <jcwong5@usf.edu>
2025-11-02 14:54:12 -0500

    Testing my ProductCard.jsx. Made product cards that display category, price, and stock. Made a button that allows the user to add to cart and made it to where the button will be disabled if the item is out of stock.

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,2 +1,19 @@
 'use client';
 
+export default function ProductCard({ product, addCart}) {
+    const outOfStock = stock =0
+
+    return (
+        <div>
+            <h3>{product.name}</h3>
+            <p>Category: {category}</p>
+            <p>Price: ${price}</p>
+            <p>Stock: {stock}</p>
+        {outOfStock ? (
+            <button disabled>Out of stock</button>
+        ):(
+            <button onClick={() => addCart(product)}>Add to cart</button>
+        )}
+        </div>
+    )
+}
